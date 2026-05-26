b6234ba1573cc65161b081cb95d3fc22f63ef0cf
AGulev <me@agulev.com>
2026-05-26 20:37:29 +0200

    udate file format

--- deffx/quad/unitquad.go
+++ deffx/quad/unitquad.go
@@ -2,21 +2,9 @@ embedded_components {
   id: "unitquad"
   type: "model"
   data: "mesh: \"/deffx/quad/quad.dae\"\n"
-  "material: \"/deffx/quad/quad.material\"\n"
-  "skeleton: \"\"\n"
-  "animations: \"\"\n"
-  "default_animation: \"\"\n"
-  "name: \"\"\n"
+  "materials {\n"
+  "  name: \"default\"\n"
+  "  material: \"/deffx/quad/quad.material\"\n"
+  "}\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
